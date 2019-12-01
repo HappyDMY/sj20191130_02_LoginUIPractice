267ed834c5d5cf559900b6b3867976e89e64b95c
jun <vagrantsj@gmail.com>
2019-12-01 10:02:43 +0900

    자바에서 로그인버튼 접근

--- app/src/main/java/com/tjoeun/sj20191130_02_loginuipractice/MainActivity.kt
+++ app/src/main/java/com/tjoeun/sj20191130_02_loginuipractice/MainActivity.kt
@@ -2,11 +2,16 @@ package com.tjoeun.sj20191130_02_loginuipractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
+
+        loginBtn.text = "Log In"
+        //loginBtn.text = "로그인"
+
+     }
 }
